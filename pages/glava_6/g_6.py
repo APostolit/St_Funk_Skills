a55bba2a136b7da958fe5948cf24ccfdb794c41a
APostolit <50382487+APostolit@users.noreply.github.com>
2026-03-16 10:58:36 +0000

    Add files via upload

---
+++ pages/glava_6/g_6.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import fun_g6
+
+# Настройка параметров данной страницы
+st.set_page_config(
+    page_title="Глава 6", # Текст на вкладке браузера
+    page_icon='📕',       # Иконка на вкладке браузера
+    layout="wide",        # Использовать всю ширину страницы
+    initial_sidebar_state="collapsed",  # Развернуть боковую панель
+)
+
+# Текст по центру страницы
+st.header("👩🏻‍💻Листинги главы 6")
+
+# Боковая панель
+with st.sidebar:
+    # Контейнер
+    cont_1 = st.container(width=300)
+
