e180ab9128f6341d09071c8f27d917270d9aa1e1
APostolit <50382487+APostolit@users.noreply.github.com>
2026-03-16 10:57:42 +0000

    Add files via upload

---
+++ pages/glava_5/g_5.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import fun_g5
+
+# Настройка параметров данной страницы
+st.set_page_config(
+    page_title="Глава 5", # Текст на вкладке браузера
+    page_icon='📕',       # Иконка на вкладке браузера
+    layout="wide",        # Использовать всю ширину страницы
+    initial_sidebar_state="collapsed",  # Развернуть боковую панель
+)
+
+# Текст по центру страницы
+st.header("👩🏻‍💻Листинги главы 5")
+
+# Боковая панель
+with st.sidebar:
+    # Контейнер
+    cont_1 = st.container(width=300)
+
